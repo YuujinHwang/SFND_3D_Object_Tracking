ee29e101c2ff6760c146d9086bb0436fb72d2ba4
yunjin <lineardept@gmail.com>
2020-04-24 12:07:34 +0900

    conflict fix

--- src/camFusion_Student.cpp
+++ src/camFusion_Student.cpp
@@ -146,12 +146,13 @@ void clusterKptMatchesWithROI(BoundingBox &boundingBox, std::vector<cv::KeyPoint
         }
     }
     double mean = accumulate(distance.begin(), distance.end(),0) / distance.size();
+    int idx = 0
     for (auto it = boundingBox.kptMatches.begin(); it != boundingBox.kptMatches.end();)
     {
         if (distance[*it]>2*mean)
         {
             boundingBox.kptMatches.erase(it);
-            distance.erase(distance.at(*it));
+            distance.erase(distance.begin+idx);
         }
         else
         {
